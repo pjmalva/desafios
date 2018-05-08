7bc55f78868c8b995818232297c5d545858ad557
Pj Malva <pjmalva@hotmail.com>
2018-05-08 10:44:11 -0300

    First One

--- desafio1.py
+++ desafio1.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
-
 """
 DESAFIO 01
-
 Encontre todos os números divisíveis por 7 mas que não são múltiplos de 5 no 
 intervalo de 2000 a 3200, inclusive.
 Imprimir esse resultado em uma só linha, separado por vírgula!
-
 """
 
-divisible_by_seven = [str(i) for i in range(2000, 3201) if i % 7 == 0 and i % 5 != 0]
-print(','.join(divisible_by_seven))
-
+d = [str(i) for i in range(2000, 3201) if i % 7 == 0 and i % 5 != 0]
+print(','.join(d))
